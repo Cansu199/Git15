d34ede781bbd833630c4db675f21663dc84855a2
Cansu199 <cansuozcan88@gmail.com>
2020-02-29 11:20:12 -0500

    add logout.java

--- src/test/Logout.java
+++ src/test/Logout.java
@@ -4,5 +4,6 @@ public class Logout {
     public static void main(String[] args) {
         System.out.println("Cansu");
         System.out.println("yasar");
+        System.out.println("Halime");
     }
 }
